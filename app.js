2ee4228c6723300f787df43d963a056d9cd83900
Leoruiz197 <ruizruizleo@hotmail.com>
2023-11-30 20:11:47 -0300

    Resultado final

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('Novo Usuario conectado');
+
+  socket.on('setUsername', (username) => {
+    socket.username = username
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', {
+      username: socket.username,
+      message: msg
+    })
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+server.listen(3001, () => {
   console.log('Servidor rodando na porta 3000');
 });
--- public/script.js
+++ public/script.js
@@ -1,1 +1,43 @@
-// Script
+const socket = io();
+let username = "";
+
+function setUsername(){
+    username = document.getElementById("username").value;
+    if (username.trim() !== "") {
+        socket.emit('setUsername', username);
+        document.getElementById("username-setter").style.display = 'none';
+        document.getElementById("chat").style.display = 'block';
+    } else {
+       alert("Por favor, digite seu username antes de continuar!"); 
+    }
+}
+
+function sendMessage() {
+    const message = document.getElementById('m').value.trim();
+    if (message !== "") {
+        socket.emit('chat message', message);
+        document.getElementById('m').value = "";
+    } else {
+        alert("digite sua mensagem antes de enviar");
+    }
+}
+
+function scrollToBottom() {
+    const chat = document.getElementById('mensagens');
+    chat.scrollTop = chat.scrollHeight;
+}
+
+socket.on('chat message', (data) => {
+    const messages = document.getElementById("messages");
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${data.username}: ${data.message}`));
+
+    if (data.username === username) {
+        li.classList.add('message-container', 'sent');
+    } else {
+        li.classList.add('message-container', 'received');
+    }
+
+    messages.appendChild(li);
+    scrollToBottom();
+})
